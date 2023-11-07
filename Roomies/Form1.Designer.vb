269c66d77d11ee16b5bbfb2a7bda95b8dc366a47
Sparsh <sonisparsh@yahoo.com>
2023-11-07 18:37:13 -0500

    Nov 10 form

--- Roomies/Form1.Designer.vb
+++ Roomies/Form1.Designer.vb
@@ -23,27 +23,60 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(307, 177)
+        Label1.Location = New Point(145, 60)
         Label1.Name = "Label1"
-        Label1.Size = New Size(159, 15)
+        Label1.Size = New Size(61, 15)
         Label1.TabIndex = 0
-        Label1.Text = "It is Good if You Can See This"' 
+        Label1.Text = "Your Tasks"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(145, 91)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(506, 101)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(145, 284)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(506, 101)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(145, 256)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(96, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Roommate Tasks"' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
         Controls.Add(Label1)
         Name = "Form1"
-        Text = "Form1"
+        Text = "Roomies"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
 End Class
